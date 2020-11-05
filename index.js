0c98a6c9cd7b71233fbc68e63467290b4e001efe
Larry <shaka422@hotmail.com>
2020-11-05 13:26:25 +0800

    feat 音量調整功能完成

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ const playButton = document.querySelector('.play-button')
 const audioElement = document.querySelector('.audio-player');
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const gainNode = audioContext.createGain();
+const oscillator = audioContext.createOscillator();
+
+const volumeControl = document.querySelector('.volume');
+const volumeText = document.querySelector('.volume-text');
+
+const detuneControl = document.querySelector('.detune');
 
 function createAudio(e) {
     const blob_url = URL.createObjectURL(e.target.files[0]);
     songTitle.innerText = `${e.target.files[0].name}`;
     audioPlayer.setAttribute('src', `${blob_url}`);
     const track = audioContext.createMediaElementSource(audioElement);
-    track.connect(gainNode).connect(audioContext.destination);
+    track.connect(oscillator).connect(gainNode).connect(audioContext.destination);
 }
 
 function playingSound() {
@@ -28,6 +34,17 @@ function playingSound() {
     }
 }
 
+function tuneVolume(e) {
+    const volume = e.target.value;
+    gainNode.gain.value = volume;
+    volumeText.innerText = `Volume : ${volume}`;
+}
+
+function tuneDetune(e) {
+    console.log(oscillator);
+    const detune = e.target.value;
+}
+
 playButton.addEventListener('click', playingSound);
 
 audioElement.addEventListener('ended', () => {
@@ -37,3 +54,6 @@ audioElement.addEventListener('ended', () => {
 updateAudio.addEventListener('input', (e) => {
     createAudio(e)
 })
+
+volumeControl.addEventListener('change', (e) => tuneVolume(e))
+detuneControl.addEventListener('change', (e) => tuneDetune(e))
