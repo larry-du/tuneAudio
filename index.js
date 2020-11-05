0079a50a56cebc125b5cdb1250f4871d5964750b
Larry <shaka422@hotmail.com>
2020-11-05 12:44:49 +0800

    feat 播放音源功能完成

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const updateAudio = document.querySelector('.update-audio');
 
 const playButton = document.querySelector('.play-button')
 const audioElement = document.querySelector('.audio-player');
-
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const gainNode = audioContext.createGain();
 
 function createAudio(e) {
     const blob_url = URL.createObjectURL(e.target.files[0]);
@@ -14,6 +14,25 @@ function createAudio(e) {
     const track = audioContext.createMediaElementSource(audioElement);
     track.connect(gainNode).connect(audioContext.destination);
 }
+
+function playingSound() {
+    if (audioContext.state === 'suspended') {
+        audioContext.resume();
+    }
+    if (this.dataset.playing === 'false') {
+        audioElement.play();
+        this.dataset.playing = 'true';
+    } else if (this.dataset.playing === 'true') {
+        audioElement.pause();
+        this.dataset.playing = 'false';
+    }
+}
+
+playButton.addEventListener('click', playingSound);
+
+audioElement.addEventListener('ended', () => {
+    playButton.dataset.playing = 'false';
+});
 
 updateAudio.addEventListener('input', (e) => {
     createAudio(e)
